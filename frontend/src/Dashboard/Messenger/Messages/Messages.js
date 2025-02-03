765b5ab869a976b109e11ea8f1a3b6342e5e14c0
kamaldbouk <kamaldbouk13@gmail.com>
2025-02-03 12:21:17 +0200

    vid 113

--- frontend/src/Dashboard/Messenger/Messages/Messages.js
+++ frontend/src/Dashboard/Messenger/Messages/Messages.js
@@ -2,6 +2,8 @@ import { useRef, useEffect } from 'react';
 import { styled } from '@mui/system';
 import MessagesHeader from './MessagesHeader';
 import { connect } from 'react-redux';
+import DUMMY_MESSAGES from './DUMMY_MESSAGES';
+import Message from './Message';
 
 const MainContainer = styled('div')({
     height: 'calc(100%-60px)',
@@ -15,6 +17,16 @@ const Messages = ({ chosenChatDetails, messages}) => {
     return ( 
         <MainContainer>
             <MessagesHeader name={chosenChatDetails?.name}/>
+            {DUMMY_MESSAGES.map((messages, index) => {
+                return <Message
+                    key={messages._id}
+                    content={messages.content}
+                    username={messages.author.username}
+                    sameAuthor={messages.sameAuthor}
+                    date={messages.date}
+                    sameDay={messages.sameDay}
+                />;
+            })}
         </MainContainer>
      );
 }
