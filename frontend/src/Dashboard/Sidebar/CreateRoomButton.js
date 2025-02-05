18bcf851bc8e7095eb652fc4bda53a896aebae8f
kamaldbouk <kamaldbouk13@gmail.com>
2025-02-05 12:29:16 +0200

    vid 139

---
+++ frontend/src/Dashboard/Sidebar/CreateRoomButton.js
@@ -0,0 +1,33 @@
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Groups';
+// import * as roomHandler from '../../realtimeCommunication/roomHandler;'
+import * as roomHandler from '../../realtimeCommunication/roomHandler';
+
+const CreateRoomButton = () => {
+
+    const createNewRoomHandler = () => {
+        //create a room and sending info to the server ab it
+        roomHandler.createNewRoom();
+    }
+    return ( 
+        <Button
+        onClick={createNewRoomHandler}
+            style={{
+                width: "48px",
+                height: "48px",
+                borderRadius: "16px",
+                margin: 0,
+                padding: 0,
+                minWidth: 0,
+                marginTop: "10px",
+                color: "white",
+                backgroundColor: "#5865F2",
+                fontSize: '25px'
+            }}
+            >
+                +
+            </Button>
+     );
+}
+ 
+export default CreateRoomButton;
---
+++ frontend/src/realtimeCommunication/roomHandler.js
@@ -0,0 +1,6 @@
+import store from "../store/store"
+import { setOpenRoom } from "../store/actions/roomActions";
+
+export const createNewRoom = () => {
+    store.dispatch(setOpenRoom(true, true))
+};
